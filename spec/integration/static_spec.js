cf9d53c14ee79d5bbc3e8f0a009b29ab02d321fa
Sean Ryan <seanpryan89@gmail.com>
2019-04-11 18:55:32 -0400

    TDD'd a route that accepts a GET request to /marco

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,23 +1,40 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marco = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
 
-//#1
+  //#1
   describe("GET /", () => {
 
-//#2
+    //#2
     it("should return status code 200", (done) => {
 
-//#3
+      //#3
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
 
-//#4
+        //#4
         done();
       });
     });
 
+  });
+
+  describe("GET /", () => {
+    it("should return status code 200", (done) => {
+      request.get(marco, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    it("should return string 'Polo'", (done) => {
+      request.get(marco, (err, res, body) => {
+        expect(body).toContain('Polo');
+        done();
+      });
+    });
   });
 });
