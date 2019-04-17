d33937a3b0f2fe4bb3623c92fea41c7646168e09
Sean Ryan <seanpryan89@gmail.com>
2019-04-17 08:28:50 -0400

    working on checkpoint topic resource, tests still not passing

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -1,14 +1,15 @@
 const topicQueries = require("../db/queries.topics.js");
 
 module.exports = {
-    index(req, res, next) {
-        topicQueries.getAllTopics((err, topics) => {
+  index(req, res, next) {
+    
+    topicQueries.getAllTopics((err, topics) => {
 
-            if (err) {
-                res.redirect(500, "static/index");
-            } else {
-                res.render("topics/index", { topics });
-            }
-        })
-    }
+      if(err){
+        res.redirect(500, "static/index");
+      } else {
+        res.render("topics/index", {topics});
+      }
+    })
+  }
 }
