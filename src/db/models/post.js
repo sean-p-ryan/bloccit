cf1bcf836988009f05134ed8b73629c7ab5ad510
Sean Ryan <seanpryan89@gmail.com>
2019-04-29 22:00:54 -0400

    create, set and get tested

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -1,25 +1,35 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  var Post = sequelize.define('Post', {
+  var Post = sequelize.define("Post", {
     title: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    body: DataTypes.STRING,
     body: {
       type: DataTypes.STRING,
       allowNull: false
     },
+
+//#2
     topicId: {
       type: DataTypes.INTEGER,
       allowNull: false
+    },
+    flairId: {
+      type: DataTypes.INTEGER,
+      allowNull: true
     }
   }, {});
   Post.associate = function(models) {
     // associations can be defined here
+
+//#3
     Post.belongsTo(models.Topic, {
       foreignKey: "topicId",
       onDelete: "CASCADE"
+    });
+    Post.belongsTo(models.Flair, {
+      foreignKey: "flairId"
     });
   };
   return Post;
